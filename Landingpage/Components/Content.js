a0a38bfd957e600175ec1c10749fa9238e4aaa70
miteshpr97 <miteshpradhan97@gmail.com>
2022-05-22 18:27:18 +0530

    initial commit

---
+++ Landingpage/Components/Content.js
@@ -0,0 +1,15 @@
+const Content = () => {
+    return (
+        <div className="content">
+            <h1>Landing page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Corporis eius,
+             officiis ea cumque ex esse dolorem debitis ad corrupti consequatur tempora quis quos nostrum culpa quibusdam.
+              Enim aut perspiciatis dignissimos?</p>
+              <br />
+             <Button />
+        </div>
+    )
+}
---
+++ Landingpage/Components/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="logo">
+                <h1>mitesh</h1>
+            </div>
+            <div className="list">
+                <li>
+                    <a href="#"> Home</a>
+                    <a href="#"> About</a>
+                    <a href="#"> Service</a>
+                    <a href="#"> Content</a>
+                </li>
+            </div>
+
+        </div>
+    )
+}
